92491d2f576d629851a94b7b502d39f91f262975
Mohamed Ismail <63530451+pkmisma@users.noreply.github.com>
2022-12-05 12:57:53 +0000

    new loadbalancer changes

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -68,7 +68,7 @@ resource "aws_security_group" "alb" {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = "0.0.0.0/0"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
 
@@ -96,7 +96,7 @@ resource "aws_lb" "sample_lb" {
          Environment = "testing"
          Role        = "Sample-Application"
     }
-    
+
     depends_on = [ aws_security_group.alb ]
 }
 
