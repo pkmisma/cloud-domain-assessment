be7515b6901d2c0aba8c0000c90b4583a1e58c33
Mohamed Ismail <63530451+pkmisma@users.noreply.github.com>
2022-12-05 22:29:46 +0000

    new loadbalancer changes

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -13,6 +13,14 @@ resource "aws_subnet" "subnet-public-1" {
     cidr_block = "10.0.1.0/24"
     map_public_ip_on_launch = "true" //it makes this a public subnet
     availability_zone = "us-east-1a"
+}
+
+resource "aws_subnet" "subnet-public-2" {
+    vpc_id = "${aws_vpc.my-vpc.id}"
+    cidr_block = "10.0.2.0/26"
+    map_public_ip_on_launch = "true" //it makes this a public subnet
+    availability_zone = "us-east-1b"
+
 
 }
 
